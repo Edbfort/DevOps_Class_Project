5077a75ab164be705a6ec87b08203749cfd5b940
Edbert Fortino <edbert.fortino@mikroskil.ac.id>
2024-07-12 17:10:12 +0700

    Revisi code

--- app/Http/Services/Public/GetProyekListService.php
+++ app/Http/Services/Public/GetProyekListService.php
@@ -12,16 +12,18 @@ class GetProyekListService
     public function handle($request)
     {
         $select = [
-            'p.id as proyek_id',
-            'p.judul_proyek as proyek_judul_proyek',
+            'proyek.id as proyek_id',
+            'proyek.judul_proyek as proyek_judul_proyek',
             'ucl.nama as client_nama',
             'uco.nama as controller_nama',
             'ut.nama as team_nama',
-            'p.id_status_proyek as proyek_status_proyek',
-            'p.perkembangan as proyek_perkembangan',
-            'p.tanggal_tegat as proyek_tanggal_tegat',
-            'p.anggaran as proyek_anggaran',
+            'proyek.id_status_proyek as proyek_status_proyek',
+            'proyek.perkembangan as proyek_perkembangan',
+            'proyek.tanggal_tegat as proyek_tanggal_tegat',
+            'proyek.anggaran as proyek_anggaran',
         ];
+
+        $where = [];
 
         $userRepo = new UserRolesRepository();
         $userRoles = $userRepo->findOneUserRolesAndNameByUserId(Auth::id());
@@ -32,32 +34,50 @@ class GetProyekListService
         if ($userRoles->nama_role == 'creative-hub-team' && !$request->has('id_user')) {
             $select = array_merge($select, [
                 'uco.lokasi as controller_lokasi',
-                'p.deskripsi_proyek as proyek_deskripsi_proyek',
-                'p.spesialisasi as proyek_spesialisasi'
+                'proyek.deskripsi_proyek as proyek_deskripsi_proyek',
+                'proyek.spesialisasi as proyek_spesialisasi'
             ]);
         } else {
             $select = array_merge($select, [
-                'db.'
+                'db.link_meeting',
+                'db.lokasi_dokumen'
             ]);
+
+            if ($userRoles->nama_role != 'controller') {
+                $where = array_merge($where, [
+                    'proyek.status = 2'
+                ]);
+
+                if ($userRoles->nama_role == 'creative-hub-team') {
+                    $where = array_merge($where, [
+                        'db.status = 1'
+                    ]);
+                }
+            }
         }
 
         $proyekQuery = Proyek::query();
-        $proyekQuery->select($select);
+        $proyekQuery->select($select)
+            ->join('users as ucl', 'ucl.id', '=', 'proyek.id_client')
+            ->leftJoin('users as uco', 'uco.id', '=', 'proyek.id_controller')
+            ->leftJoin('users as ut', 'ut.id', '=', 'proyek.id_team')
+            ->leftJoin('design_breif as db', 'db.id_proyek', '=', 'proyek.id')
+            ->where($where);
         if ($request->has('id_user')) {
             if ($request->id_user != Auth::id()) {
                 return response()->json(['message' => 'Data tidak di temukan'], 404);
             }
 
-            $proyekQuery->whereRaw($request->id_user . ' IN (p.id_client, p.id_controller, p.id_team)');
+            $proyekQuery->whereRaw($request->id_user . ' IN (proyek.id_client, proyek.id_controller, proyek.id_team)');
         } else {
             if ($request->has('keyword')) {
                 $proyekQuery->whereRaw(
                     "(ucl.nama like '%" . $request->keyword .
                     "% OR uco.nama like '%" . $request->keyword .
                     "% OR ut.nama like '%" . $request->keyword .
-                    "% OR p.spesialisasi like '%" . $request->keyword .
-                    "% OR p.judul_proyek like '%" . $request->keyword .
-                    "% OR p.deskripsi_proyek like '%" . $request->keyword .
+                    "% OR proyek.spesialisasi like '%" . $request->keyword .
+                    "% OR proyek.judul_proyek like '%" . $request->keyword .
+                    "% OR proyek.deskripsi_proyek like '%" . $request->keyword .
                     "% OR uco.lokasi like '%" . $request->keyword . ")"
                 );
             }
@@ -76,10 +96,10 @@ class GetProyekListService
                         $anggaran = [(int)$anggaran[0], (int)$anggaran[2]];
                         sort($anggaran);
 
-                        $proyekQuery->orWhereBetween('p.anggaran', $anggaran);
+                        $proyekQuery->orWhereBetween('proyek.anggaran', $anggaran);
                     } else {
                         if (!is_null($operatorArray[$anggaran[1]])) {
-                            $proyekQuery->orWhere('p.anggaran', $operatorArray[$anggaran[1]], (int)$anggaran[0]);
+                            $proyekQuery->orWhere('proyek.anggaran', $operatorArray[$anggaran[1]], (int)$anggaran[0]);
                         }
                     }
                 }
@@ -89,7 +109,7 @@ class GetProyekListService
                 $spesialisasiArray = json_decode($request->spesialisasi);
 
                 foreach ($spesialisasiArray as $spesialisasi) {
-                    $proyekQuery->orWhere('p.spesialisasi', 'like', $spesialisasi);
+                    $proyekQuery->orWhere('proyek.spesialisasi', 'like', $spesialisasi);
                 }
             }
         }
@@ -98,7 +118,7 @@ class GetProyekListService
 
         $result = [
             'proyek' => $proyek->toArray(),
-            'spesialisasi' => FilterSpesialisasi::all()->toArray(),
+            'filter_spesialisasi' => FilterSpesialisasi::all()->toArray(),
         ];
 
         return response()->json(['data' => $result, 'message' => 'Data berhasil diambil'], 200);
