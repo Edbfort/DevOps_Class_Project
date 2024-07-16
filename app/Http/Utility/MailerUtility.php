011a12d462bd097724f10074597432c1b1b69297
William <Williamalim410@gmail.com>
2024-07-16 23:46:43 +0700

    Make Utility for mailer

---
+++ app/Http/Utility/MailerUtility.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class MailerUtility
+{
+    protected $client;
+
+    public function __construct()
+    {
+        $this->client = new Client(env('MAILJET_APIKEY'), env('MAILJET_SECRETKEY'), true, ['version' => 'v3']);
+    }
+
+    public function sendEmail($recipients, $subject, $textPart, $htmlPart)
+    {
+        $body = [
+            'FromEmail' => 'vivoglobal011@gmail.com',
+            'FromName' => 'Kolab aja',
+            'Recipients' => $recipients,
+            'Subject' => $subject,
+            'Text-part' => $textPart,
+            'Html-part' => $htmlPart
+        ];
+
+        $response = $this->client->post(Resources::$Email, ['body' => $body]);
+        return $response;
+    }
+}
