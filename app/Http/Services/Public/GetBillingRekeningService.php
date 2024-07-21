5dd3e03e801abe5cee8c0424063eb6878f95cded
Edbert Fortino <edbertfortino@yahoo.com>
2024-07-21 19:42:53 +0700

    revisi get billing client

--- app/Http/Services/Public/GetBillingRekeningService.php
+++ app/Http/Services/Public/GetBillingRekeningService.php
@@ -4,29 +4,34 @@ namespace App\Http\Services\Public;
 
 use App\Models\Bank;
 use App\Models\BillingRekening;
-use App\Models\Pengguna;
-use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Symfony\Component\VarDumper\Cloner\Data;
 
 class GetBillingRekeningService
 {
     public function handle($request)
     {
-        $parameter = $request->all();
-
-        $user = Auth::id();
-
-        $bank = Bank::all();
+        $bank = Bank::all()->toArray();
 
         // find the billing rekening
-        $billingRekening = BillingRekening::select(['id_bank','nomor_rekening','nama_pemilik'])->where(['id_user' => $user])->first();
+        $billingRekening = BillingRekening::select([
+            'id_bank',
+            'nomor_rekening',
+            'nama_pemilik'
+        ])
+            ->where([
+                'id_user' => Auth::id()
+            ])
+            ->get()->toArray();
+
+        if (count($billingRekening)) {
+            $billingRekening = $billingRekening->first();
+        }
 
         $result = [
-            'bank' => $bank->toArray(),
-            'bill_rekening' =>$billingRekening ? $billingRekening->toArray(): null
+            'bank' => $bank,
+            'bill_rekening' => $billingRekening
         ];
 
-        return response()->json(['data' => $result,'message' => 'Data Billing Rekening berhasil di ambil'], 200);
+        return response()->json(['data' => $result, 'message' => 'Data Billing Rekening berhasil di ambil'], 200);
     }
 }
